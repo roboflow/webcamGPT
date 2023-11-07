c041d0588cf5e6380db1325506d3901b81c7ba4e
SkalskiP <piotr.skalski92@gmail.com>
2023-11-07 07:41:23 -0800

    Fix color space bug

--- examples/webcam.py
+++ examples/webcam.py
@@ -21,12 +21,13 @@ def save_image_to_drive(image: np.ndarray) -> str:
     image_directory = "data"
     os.makedirs(image_directory, exist_ok=True)
     image_path = os.path.join(image_directory, image_filename)
-    cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(image_path, image)
     return image_path
 
 
 def respond(image: np.ndarray, prompt: str, chat_history):
     image = np.fliplr(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image_path = save_image_to_drive(image)
     response = connector.simple_prompt(image=image, prompt=prompt)
     chat_history.append(((image_path,), None))
